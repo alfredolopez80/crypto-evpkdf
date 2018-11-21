6236fea02bb3e9ef48c96ccbb439c8d3c1c4da39
Rogelio Morrell <molekilla@gmail.com>
2018-11-21 11:48:55 -0500

    fixes

--- src/EvpKDF.spec.ts
+++ src/EvpKDF.spec.ts
@@ -1,7 +1,10 @@
+const CryptoJS = require('crypto-js')
+
 import { deriveKeyIVFromPassword } from './EvpKDF'
+import { WordArray } from './WordArray';
 describe('EvpKDF', () => {
     describe('#getEvpKDF()', () => {
-        it('should return ...', async () => {
+        it('should return key, iv and salt', async () => {
             const keySize = 256 / 32
             const ivSize = 128 / 32
             const wordArray = deriveKeyIVFromPassword(
@@ -38,6 +41,34 @@ describe('EvpKDF', () => {
             expect(wordArray.iv.words).toEqual(expectedResult.iv.words)
             expect(wordArray.key.words).toEqual(expectedResult.key.words)
             expect(wordArray.salt.words).not.toEqual(expectedResult.salt.words)
+        })
+
+        it('should return key, iv and salt', async () => {
+            const keySize = 256 / 32
+            const ivSize = 128 / 32
+            const salt = WordArray.random(64 / 8)
+            const wordArray = deriveKeyIVFromPassword(
+                'a very long password........',
+                keySize,
+                ivSize,
+                salt,
+            )
+            const derivedParams = CryptoJS.kdf.OpenSSL.execute(
+                'a very long password',
+                keySize,
+                ivSize,
+                salt,
+            )
+            const derivedParams2 = CryptoJS.kdf.OpenSSL.execute(
+                'a very long password',
+                keySize,
+                ivSize,
+                salt,
+            )
+
+
+            // expect(wordArray.key.words).toBe(derivedParams.key.words)
+            expect(derivedParams.key.words).toBe(derivedParams2.key.words)
         })
     })
 })
